7c058415a2a4d32f0dde17cdaf0c6df8e6fa0ead
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-09 12:43:08 +0200

    finish ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include "Fixed.hpp"
 
-int	main()
+int main( void )
 {
-	std::cout << "start" <<  std::endl;
-	return (0);
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
