1a8b54df79b721e45a868af99ada7ac81afce3e9
SuPuHe <mizin.a.s.2006@gmail.com>
2025-09-16 12:32:30 +0200

    copy ex01

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+const int Fixed::bit_num = 8;
+
+Fixed::Fixed(): number(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &copy)
+		this->number = copy.getRawBits();
+	return *this;
+}
+
+Fixed::Fixed(int num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	number = num << bit_num;
+}
+
+Fixed::Fixed(float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	number = roundf(num * (1 << bit_num));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits( void ) const { return number; }
+
+void Fixed::setRawBits( int const raw ) {number = raw;}
+
+float Fixed::toFloat( void ) const { return (float)number / (1 << bit_num); }
+
+int Fixed::toInt( void ) const { return number >> bit_num; }
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return out;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int					number;
+		static const int	bit_num;
+	public:
+		Fixed();
+		Fixed(Fixed const &copy);
+		Fixed &operator=(Fixed const &copy);
+		Fixed(int num);
+		Fixed(float num);
+		~Fixed();
+
+
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
